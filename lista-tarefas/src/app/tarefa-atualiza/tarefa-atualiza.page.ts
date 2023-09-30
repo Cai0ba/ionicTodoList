0c7658f089bb1a811bec589a974a0ba5c911f5db
Caioba <caio.henrique.1204@gmail.com>
2023-09-29 23:13:08 -0300

    final updates

--- lista-tarefas/src/app/tarefa-atualiza/tarefa-atualiza.page.ts
+++ lista-tarefas/src/app/tarefa-atualiza/tarefa-atualiza.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+
+import { TarefaService } from '../services/tarefa.service';
+import { ITarefa, createITarefa } from '../models/tarefa.model';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-tarefa-atualiza',
@@ -6,10 +11,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tarefa-atualiza.page.scss'],
 })
 export class TarefaAtualizaPage implements OnInit {
+  public categorias: string[] = [ ];
+  public categoriaSelecionada: string = '';
+  public categoriaIndice: number = -1;
 
-  constructor() { }
+  public tarefa: ITarefa = createITarefa();
 
-  ngOnInit() {
+  constructor(private rotaAtiva: ActivatedRoute,
+              private navCtrl: NavController,
+              private tarefaServ: TarefaService) { }
+
+  ngOnInit(): void {
+    this.categorias = this.tarefaServ.getCategorias();
+    const id: string = this.rotaAtiva.snapshot.paramMap.get('id') || '0';
+
+    console.log(id);
+
+    this.tarefa = this.tarefaServ.get(id);
+    this.categoriaSelecionada = this.tarefa.categoria;
+    this.categoriaIndice = this.categorias.findIndex((obj)=> obj === this.categoriaSelecionada);
+
+    console.log("Tarefa edit", this.tarefa);
   }
 
+  public atualizar() {
+
+    this.tarefa.categoria = this.categoriaSelecionada;
+    if (this.tarefa.nome && this.tarefa.data && this.tarefa.categoria) {
+
+      console.log('Salvar', this.tarefa);
+
+      this.tarefaServ.update(this.tarefa);
+      this.navCtrl.navigateRoot('/home');
+    } else {
+        console.log('Não é possível salvar uma tarefa vazia');
+    }
+
+  }
+
+  public selecionarCategoria(index: number) {
+    this.categoriaSelecionada = this.categorias[index];
+
+    this.categoriaIndice = index;
+
+    console.log(this.categoriaSelecionada);
+  }
 }
