39254ea8214f4a971c271c3b9d8f4633c5c43b34
Caioba <caio.henrique.1204@gmail.com>
2023-09-29 23:11:02 -0300

    home and new task implementation

--- lista-tarefas/src/app/home/home.page.ts
+++ lista-tarefas/src/app/home/home.page.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+
+import { TarefaService } from '../services/tarefa.service';
+import { ITarefa } from '../models/tarefa.model';
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.page.html',
-  styleUrls: ['./home.page.scss'],
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
 })
-export class HomePage implements OnInit {
+export class HomePage {
+  public tarefas: ITarefa[] = [ ];
 
-  constructor() { }
+  public hoje: number = Date.now();
 
-  ngOnInit() {
+  constructor(private modalCtrl: ModalController,
+              private tarefaServ: TarefaService) { }
+
+  ngOnInit(): void {
+    this.listarTarefas();
   }
 
+  public listarTarefas() {
+    this.tarefas = this.tarefaServ.getAll();
+
+    console.log(this.tarefas);
+  }
+
+  public deletar(id: string) {
+    this.tarefaServ.delete(id);
+    this.listarTarefas();
+  }
+
+  public corPrioridade(prioridade: string) {
+    return (prioridade === 'alto')? 'danger':  (prioridade === 'baixo')? 'success': 'warning';
+  }
 }
--- lista-tarefas/src/app/tarefa-nova/tarefa-nova.page.ts
+++ lista-tarefas/src/app/tarefa-nova/tarefa-nova.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+
+import { TarefaService } from '../services/tarefa.service';
+import { ITarefa, createITarefa } from '../models/tarefa.model';
 
 @Component({
   selector: 'app-tarefa-nova',
@@ -6,10 +10,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tarefa-nova.page.scss'],
 })
 export class TarefaNovaPage implements OnInit {
+  public categorias: string[] = [ ];
+  public categoriaSelecionada: string = '';
+  public categoriaIndice: number = -1;
 
-  constructor() { }
+  public tarefa: ITarefa = createITarefa();
 
-  ngOnInit() {
+  constructor(private navCtrl: NavController,
+              private tarefaServ: TarefaService) { }
+
+  ngOnInit(): void {
+   this.tarefa.prioridade = "alto";
+   this.tarefa.data = new Date().toISOString();
+   this.categorias = this.tarefaServ.getCategorias();
   }
 
+  public salvar() {
+
+    this.tarefa.categoria = this.categoriaSelecionada;
+    if (this.tarefa.nome && this.tarefa.data && this.tarefa.categoria) {
+
+      console.log('Salvar', this.tarefa);
+
+      this.tarefaServ.add(this.tarefa);
+      this.navCtrl.navigateRoot('/home');
+    } else {
+        console.log('Não é possível salvar uma tarefa vazia');
+    }
+
+  }
+
+  public selecionarCategoria(index: number) {
+    this.categoriaSelecionada = this.categorias[index];
+
+    this.categoriaIndice = index;
+
+    console.log(this.categoriaSelecionada);
+  }
 }
